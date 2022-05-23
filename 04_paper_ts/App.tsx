d13229dd7ceb67ff11a4413d60b2171508b77931
Michel <michel.tcha@gmail.com>
2022-05-23 15:05:19 +0200

    typescript ex refac update

--- 04_paper_ts/App.tsx
+++ 04_paper_ts/App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import { Appbar, DefaultTheme, Provider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import BottomNav from "./components/BottomNav";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Provider theme={theme}>
+            <SafeAreaProvider style={styles.container}>
+                <Appbar.Header>
+                    <Appbar.Content title="Title" />
+                </Appbar.Header>
+                <BottomNav />
+            </SafeAreaProvider>
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
 });
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: "#fff",
+        accent: "black",
+    },
+};
